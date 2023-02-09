026bfb706af04a89aa6861b8ce92405f753eff95
soniasalido <soniasalido@gmail.com>
2023-02-09 17:54:11 +0100

    ok

--- saneados/compruebaAcceso.php
+++ saneados/compruebaAcceso.php
@@ -33,36 +33,34 @@
 
 
     //Función que inserta el token en la base de datos
-//    function insertarToken($miToken, $miUser){
-//        try
-//        {
-//            $database = 'demos';
-//            $username = 'root';
-//            $password = '';
-//            $conn = new PDO("mysql:host=localhost:3306;dbname=$database", $username, $password);
-//            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-//            $conn-> query("SET NAMES 'utf8'");
-//            $stmt = $conn->prepare("UPDATE usuarios SET token=? WHERE User=?");
-//            $stmt->bindParam(1, $miToken);
-//            $stmt->bindParam(2, $miUser);
-//            $stmt->execute();
-//
-//            //Verificamos que el insert haya realizado
-//            if ($stmt){
-//                echo "<script>alert('Token añadido');</script>";
-//
-//            }else{
-//                echo "<script>alert('ERROR Token NO añadido');</script>";
-//            }
-//        } catch (Exception $e)
-//        {
-//            echo 'RROR!!! Excepción capturada: ',  $e->getMessage(), "\n";
-//            return false;
-//
-//        }
-//    }
+    function insertarToken($miToken, $miUser){
+        try
+        {
+            $database = 'demos';
+            $username = 'root';
+            $password = '';
+            $conn = new PDO("mysql:host=localhost:3306;dbname=$database", $username, $password);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $conn-> query("SET NAMES 'utf8'");
+            $stmt = $conn->prepare("UPDATE usuarios SET token=? WHERE User=?");
+            $stmt->bindParam(1, $miToken);
+            $stmt->bindParam(2, $miUser);
+            $stmt->execute();
 
+            //Verificamos que el insert haya realizado
+            if ($stmt){
+                echo "<script>alert('Token añadido');</script>";
 
+            }else{
+                echo "<script>alert('ERROR Token NO añadido');</script>";
+            }
+        } catch (Exception $e)
+        {
+            echo 'RROR!!! Excepción capturada: ',  $e->getMessage(), "\n";
+            return false;
+
+        }
+    }
 
 
     //Obtenemos los datos del formulario
@@ -80,7 +78,6 @@
         $_SESSION['userName'] = $_POST['user'];
         $token = md5(uniqid(rand(), TRUE));
         $_SESSION['token'] = $token;
-        include 'insertarToken.php';
         insertarToken($token, $_SESSION['userName']);
         echo "<script>window.location='menu.php'</script>";
     }else{
