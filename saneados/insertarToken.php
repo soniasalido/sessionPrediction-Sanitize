19fb2e45134572636d57a7680199a354d7e4b6bd
soniasalido <soniasalido@gmail.com>
2023-02-09 17:48:10 +0100

    ok

--- saneados/insertarToken.php
+++ saneados/insertarToken.php
@@ -1,33 +1,33 @@
 <?php
 
-// Función que inserta el token en la BD
-function insertarToken(){
-    try
-    {
-        $database = 'demos';
-        $username = 'root';
-        $password = '';
-        $conn = new PDO("mysql:host=localhost:3306;dbname=$database", $username, $password);
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $conn-> query("SET NAMES 'utf8'");
-        $stmt = $conn->prepare("UPDATE usuarios SET token=? WHERE id=?");
-        $stmt->bindParam(1, $_SESSION['token']);
-        $stmt->bindParam(2, $_SESSION['userName']);
-        $stmt->execute();
+Función que inserta el token en la base de datos
+    function insertarToken($miToken, $miUser){
+        try
+        {
+            $database = 'demos';
+            $username = 'root';
+            $password = '';
+            $conn = new PDO("mysql:host=localhost:3306;dbname=$database", $username, $password);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $conn-> query("SET NAMES 'utf8'");
+            $stmt = $conn->prepare("UPDATE usuarios SET token=? WHERE User=?");
+            $stmt->bindParam(1, $miToken);
+            $stmt->bindParam(2, $miUser);
+            $stmt->execute();
 
-        //Verificamos que el insert haya realizado
-        if ($stmt){
-            echo "<script>alert('Proceso realizado con EXITO. TOKEN añadido a la BD')</script>";
+            //Verificamos que el insert haya realizado
+            if ($stmt){
+                echo "<script>alert('Token añadido');</script>";
 
-        }else{
-            echo "<script>alert('ERROOOR. TOKEN NO añadido a la BD')</script>";
+            }else{
+                echo "<script>alert('ERROR Token NO añadido');</script>";
+            }
+        } catch (Exception $e)
+        {
+            echo 'RROR!!! Excepción capturada: ',  $e->getMessage(), "\n";
+            return false;
+
         }
-    } catch (Exception $e)
-    {
-        echo 'RROR!!! Excepción capturada: ',  $e->getMessage(), "\n";
-        return false;
-
     }
-}
 
 
