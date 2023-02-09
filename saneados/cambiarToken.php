4b54c6ca0d0dcff7420e82d8297dd57b51ec17c1
soniasalido <soniasalido@gmail.com>
2023-02-09 16:21:58 +0100

    ok

--- saneados/cambiarToken.php
+++ saneados/cambiarToken.php
@@ -2,6 +2,8 @@
 
     $token = md5(uniqid(rand(), TRUE));
     $_SESSION['token'] = $token;
+    $mierda = $token;
+    echo "mierda:" . $mierda;
 
     //echo "<script>window.location='menu.php'</script>";
     echo "<button onclick='window.location='menu.php''>Regresar</button>";
